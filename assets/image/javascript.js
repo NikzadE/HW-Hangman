69255c28effd5eaa485dfd4e50b47e0528cad0f5
NikzadE <arshad.nikzad@gmail.com>
2018-07-18 20:59:29 -0400

    firstcommit

---
+++ assets/image/javascript.js
@@ -0,0 +1,11 @@
+
+
+var choices=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+for (i=0; lenght-1; i++);
+function a () {
+console.log ("a")
+
+} 
+
+
+
